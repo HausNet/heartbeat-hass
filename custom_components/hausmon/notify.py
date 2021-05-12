2edb7cf3fc3888d7c04c0c05102f4cc7cd80074a
Louis <louis@hausnet.io>
2021-05-12 16:45:20 -0400

    WIP: Turning monitor into a sensor entity

--- custom_components/hausmon/notify.py
+++ custom_components/hausmon/notify.py
@@ -6,7 +6,8 @@ import os
 from typing import Dict, Optional
 
 from homeassistant.components.notify import BaseNotificationService
-from homeassistant.const import CONF_API_KEY, CONF_DEVICE
+from homeassistant.const import CONF_API_KEY, CONF_DEVICE, \
+    EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STARTED
 from homeassistant.helpers.event import async_call_later
 
 from hausmon_client.client import HausMonClient
@@ -50,7 +51,8 @@ class HausMonNotificationService(BaseNotificationService):
         """Initialize the service to connect to the given API url, using
         the given API key. Kicks off the HASS heartbeat timer.
         """
-        LOGGER.debug("Setting up HausMon notification service...")
+        LOGGER.debug("Creating HausMon notification entity...")
+        self._entities = []
         self._hass = hass
         domain_data: Dict = hass.data[DOMAIN]
         self._api_url: str = HAUSMON_URL
@@ -63,6 +65,24 @@ class HausMonNotificationService(BaseNotificationService):
             "Created the HausMon notification service: url=%s; device=%s",
             self._api_url, self._device_name
         )
+
+    async def async_added_to_hass(self):
+        """Subscribe to HASS started signal."""
+        self._hass.bus.async_listen(
+            EVENT_HOMEASSISTANT_STARTED,
+            self.catalog_entities
+        )
+
+    async def catalog_entities(self):
+        """Read the available entities from the registry. Called from the event
+        system after everything has been started.
+        """
+        self._entities = []
+        LOGGER.debug(
+            "Created the HausMon notification service: url=%s; device=%s",
+            self._api_url, self._device_name
+        )
+
 
     # noinspection PyUnusedLocal
     async def beat_heart(self, now=None) -> None:
@@ -92,7 +112,7 @@ class HausMonNotificationService(BaseNotificationService):
             message: str = "",
             **kwargs
     ) -> None:
-        """Send a notification via Hausmon, for the device specified. If the
+        """ Send a notification via Hausmon, for the device specified. If the
         heartbeat parameter is "True", also sends a request to reset the
         heartbeat for the device.
 
